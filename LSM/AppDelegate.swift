0455097b2caab3b07878ec940b2c3cf60684ac45
IvanVO <ivanvillanueva99@gmail.com>
2021-03-29 12:57:04 -0600

    Firebase database added w/ its corresponding cinfugration in AppDelegate.

--- LSM/AppDelegate.swift
+++ LSM/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate { 
@@ -15,6 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Override point for customization after application launch.
         UITabBar.appearance().barTintColor = .black
         UITabBar.appearance().tintColor = .red
+        FirebaseApp.configure()
         
         return true
     }
